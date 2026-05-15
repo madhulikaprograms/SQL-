8634fb4eb5b74f7085a8dcbf546eb75bec2bf617
madhulikaprograms <165761952+madhulikaprograms@users.noreply.github.com>
2026-05-16 00:51:28 +0530

    Add files via upload

---
+++ DELETE DML.sql
@@ -0,0 +1,9 @@
+-- Delete all customers with an id greater than 5
+
+DELETE FROM  customers
+WHERE id > 5
+SELECT * FROM  customers
+WHERE id > 5 
+
+-- Delete all data from the persons table
+TRUNCATE TABLE  persons
