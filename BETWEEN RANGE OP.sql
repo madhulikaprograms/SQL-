20098e12a01766f59fa78d2605df13957b884b95
madhulikaprograms <165761952+madhulikaprograms@users.noreply.github.com>
2026-05-18 00:08:14 +0530

    Add files via upload

---
+++ BETWEEN RANGE OP.sql
@@ -0,0 +1,11 @@
+/* Retrieve all customers whose score falls
+in the range between 100 and 500 */
+
+SELECT *
+FROM customers
+WHERE  score BETWEEN 100 AND 500
+
+-- by using comparision operator
+SELECT *
+FROM customers
+WHERE  score >= 100 AND score <= 500
